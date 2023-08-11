b3f1bf3d9462efb7b56d11e63e241fefb87229df
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-08-11 02:58:40 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -10,8 +9,9 @@
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t length = strlen(message);
+    char *message = "and that piece of art is useful\" - Dora Korpar, "
+                    "2015-10-19\n";
+    ssize_t length = sizeof(message) - 1;
     ssize_t half_length = length / 2;
 
     write(2, message, half_length);
