f91de1e98ff95a33e4e36a3b5209e3a563cfe9c4
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-08-11 02:44:43 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,7 @@ int main(void)
 {
 	char *message =
 	    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t length = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
-	                  - 1;
-
-	write(2, message, length);
+	ssize_t length = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, message, length - 1);
 	return (1);
 }
