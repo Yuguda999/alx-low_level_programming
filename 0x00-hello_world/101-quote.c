17ce002c98aa1b06d3ab2e87aaca6e7e23f197e4
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-08-11 01:50:34 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,11 @@
 
 /**
  * main - Entry point
- *
- * Description: Prints a specified message to standard error.
- *
- * Return: Always 1.
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t length = sizeof(message) - 1;
-
-	write(2, message, length);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
