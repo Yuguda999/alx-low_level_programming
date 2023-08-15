4367cbc683ef178a92f52c94af0f4425b22e2084
yuguda <elemenx93@gmail.com>
2023-08-15 08:35:25 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all alphabets
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
