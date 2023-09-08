efc8b25fec72eb39de600f37a0abad1ed989ed85
yuguda <elemenx93@gmail.com>
2023-09-07 17:50:54 -0700

    update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,113 +1,141 @@
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * is_digit - Checks if a string consists of digits
+ * @str: The string to check
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: 1 if all characters are digits, 0 otherwise
  */
-int _isdigit(int c)
+int is_digit(char *str)
 {
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
- */
-char *big_multiply(char *s1, char *s2)
-{
-	char *r;
-	int l1, l2, a, b, c, x;
-
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
-
-	for (l1--; l1 >= 0; l1--)
+	while (*str)
 	{
-		if (!_isdigit(s1[l1]))
-		{
-			free(r);
-			printf("Error\n"), exit(98);
-		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
 	}
-	return (r);
+	return (1);
 }
 
-
 /**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
- *
- * Return: Always 0 on success.
+ * print_error_exit - Prints "Error" and exits with status 98
  */
-int main(int argc, char **argv)
+void print_error_exit(void)
 {
-	char *r;
-	int a, c, x;
-
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
-	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
-	}
-	if (!a)
-		_putchar('0');
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
 	_putchar('\n');
-	free(r);
+	exit(98);
+}
+
+/**
+ * allocate_result_memory - Allocates memory for the result array
+ * @len: The length of the array
+ *
+ * Return: A pointer to the allocated memory
+ */
+int *allocate_result_memory(int len)
+{
+	int *result = calloc(len, sizeof(int));
+
+	if (result == NULL)
+		print_error_exit();
+	return (result);
+}
+
+/**
+ * calculate_product - Calculates the product of two numbers
+ * @result: The array to store the result
+ * @num1: The first number as a string
+ * @num2: The second number as a string
+ * @len1: The length of num1
+ * @len2: The length of num2
+ */
+void calculate_product(int *result, char *num1, char *num2, int len1, int len2)
+{
+	int i, j, carry = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int product = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+
+			result[i + j + 1] = product % 10;
+			carry = product / 10;
+		}
+		result[i + j + 1] = carry;
+	}
+}
+
+/**
+ * print_result - Prints the result array to stdout
+ * @result: The array to print
+ * @len: The length of the array
+ */
+void print_result(int *result, int len)
+{
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		if (result[i] != 0)
+			break;
+	}
+
+	if (i == len)
+		_putchar('0');
+	else
+	{
+		for (; i < len; i++)
+			_putchar(result[i] + '0');
+	}
+	_putchar('\n');
+	free(result);
+}
+
+/**
+ * multiply - Multiplies two positive numbers
+ * @num1: The first number as a string
+ * @num2: The second number as a string
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1 = 0, len2 = 0;
+	int len_result = 0;
+	int *result = allocate_result_memory(len_result);
+
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
+
+	len_result = len1 + len2;
+
+	calculate_product(result, num1, num2, len1, len2);
+
+	print_result(result, len_result);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		print_error_exit();
+
+	multiply(argv[1], argv[2]);
+
 	return (0);
 }
