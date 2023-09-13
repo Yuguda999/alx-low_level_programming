c9906395731d55fea60c81e311167de679cb871a
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-09-13 18:39:30 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,50 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- * print_opcodes - Prints the opcodes of a given memory region.
- * @start: Pointer to the start of the memory region.
- * @num_bytes: Number of bytes to print.
- */
-void print_opcodes(unsigned char *start, int num_bytes)
-{
-	int i;
-
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%.2x", start[i]);
-		if (i < num_bytes - 1)
-		{
-			printf(" ");
-		}
-	}
-	printf("\n");
-}
-
-/**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
- * Return: 0 on success, or error code on failure
- */
+ * main- Entry point
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Return: 0 in succes
+*/
 int main(int argc, char *argv[])
 {
+	int i, n;
+
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int num_bytes = atoi(argv[1]);
-
-	if (num_bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	unsigned char *main_start = (unsigned char *)main;
-	print_opcodes(main_start, num_bytes);
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
 	return (0);
 }
