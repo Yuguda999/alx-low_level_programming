b71f35f199d016391d56f84c90b311d41b90b872
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-09-13 18:33:46 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name using a callback function.
- * @name: The name to be printed.
- * @f: Function pointer that takes a char pointer as a param
+ * print_name - prints a name.
+ * @name: the name
+ * @f: the function to print with
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 		f(name);
 }
-
