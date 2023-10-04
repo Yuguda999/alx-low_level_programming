9c558adfa4d87a03d4877fd4e4a2f403ed2f194d
yuguda <elemenx93@gmail.com>
2023-10-04 15:39:00 -0700

    innitial commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef LIST_H
+#define LIST_H
+#include<stdio.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include<unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+
+#endif
