709688a472087bbe8d251079ee59c9b3afd49221
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-10-30 13:23:51 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,73 +1,41 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
 
 /**
- * read_and_write - Reads from a file descriptor and writes to standard output.
- * @fd: The file descriptor to read from.
- * @buffer: The buffer to store read data.
- * @letters: The number of letters to read and write.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: (total_written) The total number of bytes written.
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
-static ssize_t read_and_write(int fd, char *buffer, size_t letters);
 
-/**
- * read_textfile - Reads and prints a text file to standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
- *
- * Return: (total_written) The actual number of letters read and printed,
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t total_written = 0;
-	char buffer[1024];
+	 ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
 		return (0);
 
-	total_written = read_and_write(fd, buffer, letters);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	close(fd);
-
-	return (total_written);
-}
-
-static ssize_t read_and_write(int fd, char *buffer, size_t letters)
-{
-	ssize_t bytes_read, bytes_written, total_written = 0;
-
-	while (1)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		bytes_read = read(fd, buffer, sizeof(buffer));
-
-		if (bytes_read <= 0)
-			break;
-
-		if ((size_t)bytes_read > letters)
-			bytes_read = letters;
-
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			return (0);
-
-		total_written += bytes_written;
-		letters -= bytes_written;
-
-		if (letters == 0)
-			break;
+		free(buffer);
+		return (0);
 	}
 
-	if (bytes_read == -1)
-		return (0);
+	free(buffer);
+	close(o);
 
-	return (total_written);
+	return (w);
+
 }
